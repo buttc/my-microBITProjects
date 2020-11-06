7c8eca42b4493c0f10fdc6cc85aa40055c926387
buttc <conrad_68@hotmail.com>
2020-11-06 10:56:41 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,12 @@
-basic.showString("Hello!")
+let TemperatureInput = 0
+function roundToDecimal (inputTemperature: number) {
+    TemperatureInput = inputTemperature
+    basic.showString("f=")
+    basic.showString("" + (TemperatureInput))
+}
 basic.forever(function () {
-	
+    TemperatureInput = 23.56
+    basic.showString("" + (TemperatureInput))
+    roundToDecimal(TemperatureInput)
+    TemperatureInput += 1
 })
